5cbba63acf152d0a1177fc275e5d1cbb9acaaf79
Uday Nariseti <uday.nariseti@asurion.com>
2020-08-16 13:49:21 -0400

    Added app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+const {buildSchema, assertSchema }  = require('graphql');
+
+const app = express();
+const events = [];
+
+app.use(bodyParser.json());
+
+
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description:String!
+            price: Float!
+            date:String! 
+        }
+
+        input EventInput {
+            title: String!
+            description: String
+            price:Float!
+            date:String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput : EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () =>{
+            return events;
+        },
+        createEvent: (args) =>{
+            const eventName = args.name;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date().toISOString()
+            }
+            console.log(event);
+
+            events.push(event);
+            return event;
+        }
+        
+    },
+    graphiql:true
+}));
+
+app.listen(3000);
+
